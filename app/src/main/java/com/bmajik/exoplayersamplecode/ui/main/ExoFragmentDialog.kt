3c4c091ab8ebe8a44b45ed757b7b8f3f49394c32
billy anderson matjik <v-billy.matjik@anteraja.id>
2021-04-29 04:53:02 +0700

    init

--- app/src/main/java/com/bmajik/exoplayersamplecode/ui/main/ExoFragmentDialog.kt
+++ app/src/main/java/com/bmajik/exoplayersamplecode/ui/main/ExoFragmentDialog.kt
@@ -15,6 +15,7 @@ import com.bmajik.exoplayersamplecode.databinding.ExoFragmentDialogBinding
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.video.VideoListener
 
 class ExoFragmentDialog : DialogFragment() {
 
@@ -48,6 +49,7 @@ class ExoFragmentDialog : DialogFragment() {
         binding.apply {
             videoView.player = player
             videoView.useController = false
+            videoView.setKeepContentOnPlayerReset(true)
             initPlayer()
         }
     }
@@ -61,7 +63,7 @@ class ExoFragmentDialog : DialogFragment() {
 
     private fun initPlayer() {
 //        val firstVideoUri = Uri.parse("asset:///localfile.mp3")
-        val mediaItem = MediaItem.fromUri("https://r3---sn-npoe7ner.googlevideo.com/videoplayback?expire=1619373714&ei=MlqFYKezHZie4ALj_464BQ&ip=42.119.139.254&id=o-AHqa6fuokYmcDcmTKURehdq5nKgEsHiQufWQxeLyyLwy&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ns=o27nlcbVM9w22vz_6dzGD9oF&gir=yes&clen=18343878&ratebypass=yes&dur=209.142&lmt=1602466112083020&fvip=3&fexp=23886219,24001373,24007246&beids=23886219&c=WEB&txp=5316222&n=mUPEXDWYy7Oy9v1Swb&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgVWKTZLv-Rae1e31ZN8-aAmbo8yoUpuoirXovth1o6CACIQDF35z-d5BlR-wDhRNDbsA2bSzJw_C2iL1coPoRGxUFNg%3D%3D&rm=sn-42u-nbos67s,sn-nposy7s&req_id=e76a0ce4df43a3ee&redirect_counter=2&cms_redirect=yes&ipbypass=yes&mh=X_&mip=101.128.127.157&mm=30&mn=sn-npoe7ner&ms=nxu&mt=1619352739&mv=m&mvi=3&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhALWoxLmRFImdGJsDTbUzV5msEaKRE_vXslJAkgr9e4SxAiAeYPMcxNKecS5YINKQF4TRrWLdjjKqBaUW-JpyoYQ-9A%3D%3D")
+        val mediaItem = MediaItem.fromUri("https://r3---sn-npoeenl7.googlevideo.com/videoplayback?expire=1619666971&ei=utOJYLD1OcGjx_AP4NSZsAU&ip=45.153.33.166&id=o-ACErwMUJOOu9urXWsYfjqMBk8wvmaHWQ5qaUX7EtUtue&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ns=gadJpqL0dy_ArKPAPnsFw-QF&gir=yes&clen=18343878&ratebypass=yes&dur=209.142&lmt=1602466112083020&fvip=3&fexp=24001373,24007246&c=WEB&txp=5316222&n=ejHAGWM8nPRNon5&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhAPGF3ECqd1pD9KYPjnVF0eZa_TkO2t_PPHUHbVXZEehBAiAZCGzsCXv5cDAFr54kHmnWOZDIOELR8IFcYMLq9ks5tQ%3D%3D&rm=sn-5hnes77z&req_id=8ed2029c16ffa3ee&ipbypass=yes&redirect_counter=2&cm2rm=sn-jcopn2-jb3l7s&cms_redirect=yes&mh=X_&mip=101.128.127.164&mm=29&mn=sn-npoeenl7&ms=rdu&mt=1619645320&mv=m&mvi=3&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAK6aR9q695W4sa_BaYFFsIlKMIAUJkCsc4pHkT6gGSCvAiBmov9qa5sKPgBQYG-co2mrk9qYxR-sSWK1mywQFvbxKA%3D%3D")
         player.setMediaItem(mediaItem)
         player.playWhenReady = playWhenReady
         player.prepare();
